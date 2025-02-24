b34636e2f241e82562396167c6d17b09c200482a
USHER-PB <ushertchankoumi9@gmail.com>
2025-02-24 11:42:44 +0100

    feat added:logging info
    Signed-off-by: USHER-PB <ushertchankoumi9@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 // In src/lib.rs
 pub mod input;
 pub mod error;
+
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use structopt::StructOpt;
-
-use lambers_w::input::{take_value, CLI};
-use lambers_w::error::Error;
-    
 mod input;
 mod error;
-
+use log::info;
 fn main() {
   env_logger::init();
+  info!("usher what is ya problem");
   let opt = input::CLI::from_args();
   
   let output = input::take_value(opt.x);
